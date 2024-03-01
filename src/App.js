af1a18547aa9590882df58892ac7c59599b96972
ACER <ACER@IMTHANG>
2024-03-01 21:10:40 +0700

    P10.1.2 -  Validate Form React

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import Login from "./components/Login/Login";
 import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Register from "./components/Register/Register";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Router>
-      <div className="app-contanier">
-        {/* <Nav /> */}
-        <Switch>
-          <Route path="/news">news</Route>
-          <Route path="/about">about</Route>
-          <Route path="/contact">contact</Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="*">404 Not Found</Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="app-contanier">
+          {/* <Nav /> */}
+          <Switch>
+            <Route path="/news">news</Route>
+            <Route path="/about">about</Route>
+            <Route path="/contact">contact</Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/" exact>
+              Home
+            </Route>
+            <Route path="*">404 Not Found</Route>
+          </Switch>
+        </div>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,19 +1,61 @@
 import "./Register.scss";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   let history = useHistory();
   const handleLogin = () => {
     history.push("/login");
   };
 
   useEffect(() => {
-    axios.get("http://localhost:8080/api/test-api").then((data) => {
-      console.log(">>> check data axios: ", data);
-    });
+    // axios.get("http://localhost:8080/api/test-api").then((data) => {
+    //   console.log(">>> check data axios: ", data);
+    // });
   }, []);
+
+  const isValidInput = () => {
+    if (!email) {
+      toast.error("Email is required");
+      return false;
+    }
+    let regx = /\S+@\S+\.\S+/;
+    if (!regx.test(email)) {
+      toast.error("Please enter a valid email address");
+      return false;
+    }
+    if (!phone) {
+      toast.error("Phone is required");
+      return false;
+    }
+    if (!password) {
+      toast.error("Password is required");
+      return false;
+    }
+
+    if (password != confirmPassword) {
+      toast.error("Your password is not the same");
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleRegister = () => {
+    let check = isValidInput();
+
+    let userData = { email, phone, username, password };
+    console.log(">>> check user data: ", userData);
+  };
 
   return (
     <div className="register-contanier">
@@ -32,6 +74,8 @@ const Register = (props) => {
                 type="text"
                 className="form-control"
                 placeholder="Email address"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
               />
             </div>
 
@@ -41,6 +85,8 @@ const Register = (props) => {
                 type="text"
                 className="form-control"
                 placeholder="Phone number"
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
               />
             </div>
 
@@ -50,6 +96,8 @@ const Register = (props) => {
                 type="text"
                 className="form-control"
                 placeholder="Username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
               />
             </div>
 
@@ -59,6 +107,8 @@ const Register = (props) => {
                 type="password"
                 className="form-control"
                 placeholder="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
               />
             </div>
 
@@ -68,10 +118,18 @@ const Register = (props) => {
                 type="password"
                 className="form-control"
                 placeholder="Re-enter Password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
               />
             </div>
 
-            <button className="btn btn-primary">Register</button>
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={() => handleRegister()}
+            >
+              Register
+            </button>
             <hr />
             <div className="text-center">
               <button className="btn btn-success" onClick={() => handleLogin()}>
